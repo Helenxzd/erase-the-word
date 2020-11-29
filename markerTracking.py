702fec0d9b0076dc7792e5db08f8e6c63dbcc559
duxinzhe <naomixzdu@gmail.com>
2020-11-29 18:38:06 -0500

    update with videos considering the position of reference

--- markerTracking.py
+++ markerTracking.py
@@ -13,21 +13,31 @@ def generateTileSet():
     # Initialize candidate
     tilCandidate = np.zeros((55, 5, 3), np.uint8)
 
-    for x in range(125, 167, 2):
-        for y in range(15, 255, 2):
+    for x in range(125, 145, 2):
+        for y in range(15, 200, 2):
             tilCandidate = img[x:x + 55, y:y + 5]
             tileSet.append(tilCandidate)
 
     return tileSet
 
 def findCandidate(img, curW, tileSet):
-    currentPatch = img[125:180, curW:curW + 5]
+    # bottom
+    # currentPatch = img[125:180, curW:curW + 5]
+    # right
+    # currentPatch = img[70:125, curW+5:curW + 10]
+    # top
+    # currentPatch = img[25:80, curW+5:curW+10]
+    currentPatchBottom = img[125:180, curW:curW + 5]
+    currentPatchRight = img[70:125, curW + 5:curW + 10]
+
 
     min_ssd = sys.maxsize
     index = -1
     for i in range(len(tileSet)):
 
-        ssd = np.sum(np.square(currentPatch - tileSet[i]))
+        ssd_bottom = np.sum(np.square(currentPatchBottom - tileSet[i]))
+        ssd_right = np.sum(np.square(currentPatchRight - tileSet[i]))
+        ssd = ssd_bottom + ssd_right
         if ssd < min_ssd:
             min_ssd = ssd
             index = i
@@ -49,11 +59,11 @@ def tracking(frames, tileSet, mode = "ssd"):
 
 
     if mode == "ssd":
-        file_path = "./test1.mp4"
+        file_path = "./test_bottom_right.mp4"
         size = (fst_img.shape[1], fst_img.shape[0])
         out = cv2.VideoWriter(file_path, cv2.VideoWriter_fourcc(*'XVID'), fps, size)
 
-        for i in range(1, 228):
+        for i in range(1, len(frames)):
             src = cv2.imread(frames[i])
             img = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
             min_ssd = sys.maxsize
@@ -84,7 +94,7 @@ def tracking(frames, tileSet, mode = "ssd"):
             out.write(src)
         out.release()
 
-tileSet = generateTileSet()
 
 frames = sorted(glob.glob('/Users/duxinzhe/PycharmProjects/magic-eraser-cv/videoImage/*.jpg'))
+tileSet = generateTileSet()
 tracking(frames, tileSet, mode = "ssd")
